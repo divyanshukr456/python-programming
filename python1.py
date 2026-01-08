2b8ea00796675324951f11060ba5b8e2814faf18
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-01-08 11:09:45 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,53 @@
+#assignment operators
+#num =10
+#num=num+10
+#num+=10
+#print("num:",num)
+
+#logical operators 
+#a=50
+#b=20
+
+# val1=True
+
+# a=4
+# b=4
+# diff=a-b
+# print(diff)
+# name=input("enter your name:")
+# print("welcome",name)
+# name=input("enter name")
+# age=input("enter age")
+# grade=input("enter grade")
+# print("welcome",name)
+# print("your age",age)
+# print("your grade",grades)
+# a=2
+# b=6
+# sum=a+b
+# print(sum)
+# side=int (input("Enter side:"))
+
+# print("area=",side*side)
+# First =float(input("Enter First value :" ))
+                   
+# second=float(input("Enter the second value :"))
+# sum=First+second
+# print("Average=",sum/2)
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+
+print("OR operator =",(a>b)or(a==b))
+
+
+
+
+
+
+
+
+
+
+
+
+
