e8e5d14580dfba49ae82a72daa8bdb2678b11da0
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-01-10 00:21:28 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the number a :"))
+b=int(input("Enter the number b :"))
+c=int(input ("Enter the number c :"))
+d=int(input("Enter the number d :40"))
+
+if a>b and a>c and a>d:
+    print("a is greatest")
+elif b>a and b>c and b>d:
+    print("b is greatest")
+
+elif c>a and c>b and c>d:
+    print("c is greater")
+else:
+    print("d is greastet")
+
