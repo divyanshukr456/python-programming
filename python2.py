ea8874a842c23e5e03333e90140485de92d8f9aa
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-01-14 23:28:43 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,29 @@
+# light=input("Enter light color=")
+# if(light=="red"):
+#     print("stop")
+# elif(light=="yellow"):
+#     print("go slow")
+# elif(light=="green"):
+#    print("g0 fast")
+# else:
+#     print("light is broken")
+
+age=int(input("Enter your age"))
+if(age>=18):
+    print("eligible for voting")
+else:
+    print("Not eligible for voting")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python3.py
@@ -0,0 +1,26 @@
+# marks1=98.4
+# marks2=90.4
+# marks3=90.2
+# marks4=10.2
+
+# marks=[94.4,97.5,60.7,34.5]
+# print(marks)
+# print(type(marks)) #marks[0]
+# print(marks[1])
+
+# student=["karan ,divyanshu,keshav"]
+# list=['a','c','b','h','y']
+
+# list.sort(reverse=True)
+# print(list)
+
+
+
+
+
+
+
+
+
+
+
