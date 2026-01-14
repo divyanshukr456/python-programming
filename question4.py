ab63ac4616a92d74483a205bd8de461ca907bbd3
Divyanshu Kumar Sharma <skylife4098@gmail.com>
2026-01-14 23:28:06 +0530

    Add files via upload

---
+++ question4.py
@@ -0,0 +1,10 @@
+movies=[]
+movies.append(input("enter 1st movie:"))
+
+movies.append(input("enter 2nd movie:"))
+
+movies.append(input("enter 3rd movie:"))
+
+print(movies)
+
+
---
+++ question7.py
@@ -0,0 +1,3 @@
+grade=["1","4","7","5","8","3","0"]
+grade.sort()
+print(grade)
